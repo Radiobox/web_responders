1f5d49bb43e9b1f42fea887ac8c622afe45dbbf8
Samuel Nelson <valczir.darkvein@gmail.com>
2014-02-04 14:37:44 -0700

    Renamed ResponseCreator Interface, Added Documentation
    ResponseCreator was misleading as a name for said interface, since it's not used to create a response.  It's only used to create a value within a response.  I'm sure I could pick an even less misleading name, but this works for now.

--- controllers.go
+++ controllers.go
@@ -4,6 +4,11 @@ import (
 	"github.com/stretchr/goweb/context"
 )
 
+// A BaseRestController is just a controller that always sets the Vary
+// header to "Accept", since most REST APIs will change their response
+// based on the Accept header.  All this really does is tell clients,
+// "If your Accept header changes, you shouldn't use the cached
+// value."
 type BaseRestController struct{}
 
 // After makes sure that the Vary header is set to Accept, after the
--- responders.go
+++ responders.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/goweb"
 	"github.com/stretchr/goweb/context"
 	"github.com/stretchr/objx"
+	"github.com/Radiobox/model_helpers"
 	"reflect"
 	"strings"
 	"unicode"
@@ -19,17 +20,30 @@ import (
 const SqlNullablePrefix = "Null"
 
 // ResponseCreator is a datatype that prefers to create its own
-// response value when used as a value within a response, rather than
-// being automatically parsed into a sub-element.
-type ResponseCreator interface {
-	Response() interface{}
+// response value when it is used as a piece of a response (i.e. in a
+// struct field, or as a value in a slice or map).
+//
+// This is particularly useful for compressing a large struct to
+// something much smaller in two situations:
+//
+// 1. You are responding with a list of *many* of the large struct
+// type, often as part of a `GET /resource` response.
+// 2. You are responding with a struct type that includes the large
+// struct type as a field, often as part of a `GET /otherresource/id`
+// response.
+type ResponseValueCreator interface {
+
+	// ResponseValue should return the value that will be used to
+	// represent the underlying value in a response.
+	ResponseValue() interface{}
 }
 
 // MessageMap is a map intended to be used for carrying messages
 // around, for the purpose of error handling.  Methods on MessageMap
 // always expect the MessageMap to already contain the keys "err",
 // "warn", and "info"; and for each of those to contain a slice of
-// strings.
+// strings.  You can use NewMessageMap() to set up an empty MessageMap
+// value.
 type MessageMap map[string][]string
 
 // NewMessageMap returns a MessageMap that is properly initialized.
@@ -45,40 +59,84 @@ func (mm MessageMap) addMessage(severity, message string) {
 	mm[severity] = append(mm[severity], message)
 }
 
+// AddErrorMessage adds an error message to the message map.
 func (mm MessageMap) AddErrorMessage(message string) {
 	mm.addMessage("err", message)
 }
 
+// Errors returns a slice of all the error messages that have been
+// added to this message map.
 func (mm MessageMap) Errors() []string {
 	return mm["err"]
 }
 
+// AddWarningMessage adds a warning message to the message map.
 func (mm MessageMap) AddWarningMessage(message string) {
 	mm.addMessage("warn", message)
 }
 
+// Warnings returns a slice of all warning messages that have been
+// added to this message map.
 func (mm MessageMap) Warnings() []string {
 	return mm["warn"]
 }
 
+// AddInfoMessage adds an info message to this message map.
 func (mm MessageMap) AddInfoMessage(message string) {
 	mm.addMessage("info", message)
 }
 
+// Infos returns a slice of all info messages that have been added to
+// this message map.
 func (mm MessageMap) Infos() []string {
 	return mm["info"]
 }
 
+// NumErrors is sugar for len(MessageMap.Errors())
 func (mm MessageMap) NumErrors() int {
 	return len(mm.Errors())
 }
 
+// NumErrors is sugar for len(MessageMap.Warnings())
 func (mm MessageMap) NumWarnings() int {
 	return len(mm.Warnings())
 }
 
+// NumErrors is sugar for len(MessageMap.Infos())
 func (mm MessageMap) NumInfos() int {
 	return len(mm.Infos())
+}
+
+// CreateResponse takes a value to be used as a response and attempts
+// to generate a value to respond with, based on struct tag and
+// interface matching.
+//
+// Struct values will be converted to a map[string]interface{}.  Each
+// field will be assigned a key - the "request" tag's value if it
+// exists, or the "response" tag's value if it exists, or just the
+// lowercase field name if neither tag exists.  A value of "-" for the
+// key (i.e. the value of a request or response tag) will result in
+// the field being skipped.
+//
+// CreateResponse will skip parsing any sub-elements of a response
+// (i.e. entries in a slice or map, or fields of a struct) that
+// implement the ResponseValueCreator, and instead just use the return
+// value of their ResponseValue() method.
+func CreateResponse(data interface{}) interface{} {
+	value := reflect.ValueOf(data)
+	if value.Kind() == reflect.Ptr {
+		value = value.Elem()
+	}
+	switch value.Kind() {
+	case reflect.Struct:
+		return createStructResponse(value)
+	case reflect.Slice, reflect.Array:
+		return createSliceResponse(value)
+	case reflect.Map:
+		return createMapResponse(value)
+	default:
+		return data
+	}
 }
 
 // createNullableDbResponse checks for "database/sql".Null* types, or
@@ -110,32 +168,8 @@ func createNullableDbResponse(value reflect.Value, valueType reflect.Type) (inte
 	return nil, errors.New("No Nullable DB value found")
 }
 
-// createResponse takes a value to be used as a response and attempts
-// to generate a map of values, to be used in the response, from the
-// value's fields.  It returns an empty map if the passed in value is
-// not a struct.
-//
-// It uses the "response" struct tag for naming purposes when
-// converting from a struct type to a map[string]interface{} type - if
-// a field has a "response" tag, that will be used for the index in
-// the response; otherwise, the lowercase field name will be used.
-func createResponse(data interface{}) interface{} {
-	value := reflect.ValueOf(data)
-	if value.Kind() == reflect.Ptr {
-		value = value.Elem()
-	}
-	switch value.Kind() {
-	case reflect.Struct:
-		return createStructResponse(value)
-	case reflect.Slice, reflect.Array:
-		return createSliceResponse(value)
-	case reflect.Map:
-		return createMapResponse(value)
-	default:
-		return data
-	}
-}
-
+// createMapResponse is a helper for generating a response value from
+// a value of type map.
 func createMapResponse(value reflect.Value) interface{} {
 	response := reflect.MakeMap(value.Type())
 	for _, key := range value.MapKeys() {
@@ -145,6 +179,8 @@ func createMapResponse(value reflect.Value) interface{} {
 	return response.Interface()
 }
 
+// createSliceResponse is a helper for generating a response value
+// from a value of type slice.
 func createSliceResponse(value reflect.Value) []interface{} {
 	response := make([]interface{}, 0, value.Len())
 	for i := 0; i < value.Len(); i++ {
@@ -154,10 +190,55 @@ func createSliceResponse(value reflect.Value) []interface{} {
 	return response
 }
 
+// createStructResponse is a helper for generating a response value
+// from a value of type struct.
+func createStructResponse(value reflect.Value) interface{} {
+	structType := value.Type()
+
+	// Support "database/sql".Null* types, and any other types
+	// matching that structure
+	if v, err := createNullableDbResponse(value, structType); err == nil {
+		return v
+	}
+
+	response := make(map[string]interface{})
+
+	for i := 0; i < value.NumField(); i++ {
+		fieldType := structType.Field(i)
+		fieldValue := value.Field(i)
+
+		if fieldType.Anonymous {
+			embeddedResponse := CreateResponse(fieldValue.Interface()).(map[string]interface{})
+			for key, value := range embeddedResponse {
+				// Don't overwrite values from the base struct
+				if _, ok := response[key]; !ok {
+					response[key] = value
+				}
+			}
+		} else if unicode.IsUpper(rune(fieldType.Name[0])) {
+			responseValue := createResponseValue(fieldValue)
+
+			name := fieldType.Tag.Get("response")
+			switch name {
+			case "-":
+				continue
+			case "":
+				name = strings.ToLower(fieldType.Name)
+				fallthrough
+			default:
+				response[name] = responseValue
+			}
+		}
+	}
+	return response
+}
+
+// createResponseValue is a helper for generating a response value for
+// a single value in a response object.
 func createResponseValue(value reflect.Value) (responseValue interface{}) {
 	switch source := value.Interface().(type) {
-	case ResponseCreator:
-		responseValue = source.Response()
+	case ResponseValueCreator:
+		responseValue = source.ResponseValue()
 	case fmt.Stringer:
 		responseValue = source.String()
 	case error:
@@ -185,49 +266,12 @@ func createResponseValue(value reflect.Value) (responseValue interface{}) {
 	return
 }
 
-func createStructResponse(value reflect.Value) interface{} {
-	structType := value.Type()
-
-	// Support "database/sql".Null* types, and any other types
-	// matching that structure
-	if v, err := createNullableDbResponse(value, structType); err == nil {
-		return v
-	}
-
-	response := make(map[string]interface{})
-
-	for i := 0; i < value.NumField(); i++ {
-		fieldType := structType.Field(i)
-		fieldValue := value.Field(i)
-
-		if fieldType.Anonymous {
-			embeddedResponse := createResponse(fieldValue.Interface()).(map[string]interface{})
-			for key, value := range embeddedResponse {
-				// Don't overwrite values from the base struct
-				if _, ok := response[key]; !ok {
-					response[key] = value
-				}
-			}
-		} else if unicode.IsUpper(rune(fieldType.Name[0])) {
-			responseValue := createResponseValue(fieldValue)
-
-			name := fieldType.Tag.Get("response")
-			switch name {
-			case "-":
-				continue
-			case "":
-				name = strings.ToLower(fieldType.Name)
-				fallthrough
-			default:
-				response[name] = responseValue
-			}
-		}
-	}
-	return response
-}
-
+// Respond performs an API response, adding some additional data to
+// the context's CodecOptions to support our custom codecs.  This
+// particular function is very specifically for use with the
+// github.com/stretchr/goweb web framework.
 func Respond(ctx context.Context, status int, notifications MessageMap, data interface{}) error {
-	params, err := ParseParams(ctx)
+	params, err := model_helpers.ParseParams(ctx)
 	if err != nil {
 		return err
 	}
@@ -238,7 +282,7 @@ func Respond(ctx context.Context, status int, notifications MessageMap, data int
 		"notifications": notifications,
 	})
 
-	data = createResponse(data)
+	data = CreateResponse(data)
 
 	return goweb.API.WriteResponseObject(ctx, status, data)
 }
